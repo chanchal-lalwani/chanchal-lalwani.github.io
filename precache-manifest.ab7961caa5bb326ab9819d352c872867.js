5393696f8658478bfb9007bd790087eabb574269
Chanchal Lalwani <clalwani522@gmail.com>
2020-07-13 18:21:58 +0530

    Create React App

---
+++ precache-manifest.ab7961caa5bb326ab9819d352c872867.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "486de05389040a26ebddf117483886da",
+    "url": "/index.html"
+  },
+  {
+    "revision": "10534970c66d48c94459",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "8d61282cf4c955ac0897",
+    "url": "/static/js/2.d75ba349.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.d75ba349.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "10534970c66d48c94459",
+    "url": "/static/js/main.f3542b7d.chunk.js"
+  },
+  {
+    "revision": "994fdd3f1c8a9ee904f8",
+    "url": "/static/js/runtime-main.fe11bc43.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
